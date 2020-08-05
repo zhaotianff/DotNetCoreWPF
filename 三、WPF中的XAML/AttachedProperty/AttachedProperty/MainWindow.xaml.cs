dac0fb17907257ba932b38df6a4588ac6a090b63
ZTI <zhaotianff@163.com>
2020-08-05 11:33:59 +0800

    增加了一些示例代码

---
+++ 三、WPF中的XAML/AttachedProperty/AttachedProperty/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttachedProperty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Test()
+        {
+            /*
+             *  <Grid>
+                    <Grid.RowDefinitions>
+                        <RowDefinition/>
+                        <RowDefinition/>
+                    </Grid.RowDefinitions>
+
+                    <Grid.ColumnDefinitions>
+                        <ColumnDefinition/>
+                        <ColumnDefinition/>
+                    </Grid.ColumnDefinitions>
+
+                    <!--通过附加属性来设置在Grid的哪一行，哪一列-->
+                    <Label Content="HelloWorld" Grid.Row="1" Grid.Column="1"/>
+                </Grid>
+            */
+
+            //对应的C#代码如下
+            Grid grid = new Grid();
+
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            Label label = new Label();
+            label.Content = "HelloWorld";
+
+            //附加属性被转换成以下代码 
+            Grid.SetRow(label, 1);
+            Grid.SetColumn(label, 1);
+
+            grid.Children.Add(label);
+
+        }
+    }
+}
---
+++ 三、WPF中的XAML/NestedElements/NestedElements/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NestedElements
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TestMethod()
+        {
+            /*
+             * <Window>
+                   <Grid>
+                        <Button Content="OK"/>
+                        <Label Content="label"/>
+                        <TextBox/>
+                   </Grid>
+               </Window>
+            */
+
+            //以上XAML代码实际对应以下C#代码
+
+            Grid grid = new Grid();
+
+            Button button = new Button();
+            button.Content = "OK";
+
+            Label label = new Label();
+            label.Content = "label";
+
+            TextBox textBox = new TextBox();
+
+            grid.Children.Add(button);
+            grid.Children.Add(label);
+            grid.Children.Add(textBox);
+
+            this.Content = grid;
+            
+        }
+    }
+}
