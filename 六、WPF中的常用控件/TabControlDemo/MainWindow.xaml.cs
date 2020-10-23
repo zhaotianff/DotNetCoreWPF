5370c4acc6f5e85d600359e1531fa518b5cf7c81
zti <zhaotianff@163.com>
2020-10-23 17:25:37 +0800

    更新了TabControl示例

---
+++ 六、WPF中的常用控件/TabControlDemo/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabControlDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tab.Items.Add(new TabItem() { Header = "Added Item" });
+        }
+
+        private void tab_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabControl ti = (sender as TabControl);
+            this.Dispatcher.BeginInvoke(new Action(()=> {
+                MessageBox.Show(ti.SelectedIndex.ToString());
+            })); 
+        }
+    }
+}
