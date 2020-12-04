541d88e0ddb7845f59a2268f05027a912ba87c2a
zti <zhaotianff@163.com>
2020-12-04 18:18:49 +0800

    更新Window类示例代码

---
+++ 七、WPF中的常用控件(二)/WindowDemo/TransparencyWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowDemo
+{
+    /// <summary>
+    /// Interaction logic for TransparencyWindow.xaml
+    /// </summary>
+    public partial class TransparencyWindow : Window
+    {
+        public TransparencyWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
