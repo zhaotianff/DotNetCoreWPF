2248e05caa1375d043dcd9dfda209a6fcfd3fe83
zti <zhaotianff@163.com>
2020-12-05 12:01:10 +0800

    更新了窗口示例代码

--- 七、WPF中的常用控件(二)/WindowDemo/MainWindow.xaml.cs
+++ 七、WPF中的常用控件(二)/WindowDemo/MainWindow.xaml.cs
@@ -30,5 +30,35 @@ namespace WindowDemo
             TransparencyWindow transparencyWindow = new TransparencyWindow();
             transparencyWindow.Show();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //使用XAML布局
+            XAMLWindow xAMLWindow = new XAMLWindow();
+            xAMLWindow.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //使用代码创建 
+            Window window = new Window();
+            window.Width = 720;
+            window.Height = 500;
+            window.Background = Brushes.LightSkyBlue;
+            window.Title = "Window 1";
+
+            Grid grid = new Grid();
+
+            Label label = new Label();
+            label.HorizontalAlignment = HorizontalAlignment.Center;
+            label.VerticalAlignment = VerticalAlignment.Center;
+            label.Content = "Hello World";
+            label.FontSize = 30;
+            grid.Children.Add(label);
+
+            window.Content = grid;
+
+            window.ShowDialog();
+        }
     }
 }
