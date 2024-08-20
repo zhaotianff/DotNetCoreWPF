c878d4c457bf6d8e07c7f64804ff4788843b6f1f
zhaotianff <zhaotianff@163.com>
2024-08-20 17:24:27 +0800

    增加逻辑树和可视化树示例

---
+++ 未分类、逻辑树和可视化树/LogicalTreeAndVisualTree/LogicalTreeAndVisualTree/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LogicalTreeAndVisualTree
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 未分类、逻辑树和可视化树/LogicalTreeAndVisualTree/LogicalTreeAndVisualTree/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace LogicalTreeAndVisualTree
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void PrintLogicalTree(object parent, int level)
+        {
+            var parentObj = parent as DependencyObject;
+            var typeName = parent.GetType().FullName;
+            var name = "";
+            if (parentObj != null)
+            {
+                name = (string)(parentObj.GetValue(FrameworkElement.NameProperty) ?? "");
+            }
+            else
+            {
+                name = parent.ToString();
+            }
+
+            AppendText(GetIndentString().Substring(0, level * 2));
+            AppendText($"{typeName}:", true);
+            AppendText($" {name}");
+            AppendText(Environment.NewLine);
+
+            if (parentObj == null)
+                return;
+
+            var children = LogicalTreeHelper.GetChildren(parentObj);
+            foreach (object child in children)
+            {
+                PrintLogicalTree(child, level + 1);
+            }
+        }
+
+        public void PrintVisualTree(DependencyObject parent, int level)
+        {
+            string typeName = parent.GetType().FullName ?? parent.GetType().Name;
+            string name = (string)(parent.GetValue(FrameworkElement.NameProperty) ?? "");
+            AppendText(GetIndentString().Substring(0, level * 2));
+            AppendText($"{typeName}:",true);
+            AppendText($" {name}");
+            AppendText(Environment.NewLine);
+            for (int i = 0; i != VisualTreeHelper.GetChildrenCount(parent); ++i)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                PrintVisualTree(child, level + 1);
+            }
+        }
+
+        private void AppendText(string text,bool isbold = false)
+        {
+            if (this.rtbox.Document.Blocks.Count == 0)
+            {
+                Paragraph paragraph = new Paragraph(isbold ? new Bold(new Run(text)) : new Run(text));
+                this.rtbox.Document.Blocks.Add(paragraph);
+            }
+            else
+            {
+                (this.rtbox.Document.Blocks.ElementAt(0) as Paragraph)?.Inlines.Add(isbold ? new Bold(new Run(text)) : new Run(text));
+            }
+        }
+
+        private void ClearText()
+        {
+            this.rtbox.Document.Blocks.Clear();
+        }
+
+        private void GetVisualTree_Click(object sender, RoutedEventArgs e)
+        {
+            ClearText();
+            PrintVisualTree(this.grid, 0);
+        }
+
+        private void GetLogicalTree_Click(object sender, RoutedEventArgs e)
+        {
+            ClearText();
+            PrintLogicalTree(this.grid, 0);
+        }
+
+        private string GetIndentString()
+        {
+            return " ".PadLeft(100, ' ');
+        }
+
+        private void GetChildViaLogicalTree_Click(object sender, RoutedEventArgs e)
+        {
+            this.grid.FindName()
+        }
+    }
+}
